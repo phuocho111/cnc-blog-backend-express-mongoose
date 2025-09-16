1503d7019cfafdeeec117f96c1d70f24412e26ac
phuocht <phuocht@d-soft.com.vn>
2025-09-16 16:44:47 +0700

    update/swagger

--- src/swagger.js
+++ src/swagger.js
@@ -3,15 +3,32 @@ const YAML = require("yamljs");
 
 // Tạo specs
 const userSpec = YAML.load("./src/docs/user.yaml");
+const postSpec = YAML.load("./src/docs/post.yaml");
 
 function swaggerDocs(app, port) {
   // route swagger
-  app.use("/docs/Users", swaggerUi.serve, swaggerUi.setup(userSpec));
+  app.use(
+    "/docs",
+    swaggerUi.serve,
+    swaggerUi.setup(null, {
+      explorer: true, // bật chọn definition
+      swaggerOptions: {
+        urls: [
+          { url: "/swagger/user.json", name: "User API" },
+          { url: "/swagger/post.json", name: "Post API" },
+        ],
+      },
+    }),
+  );
 
   //Docs in JSON format
-  app.use((req, res) => {
+  app.use("/swagger/user.json", (req, res) => {
     res.setHeader("Content-Type", "application/json");
     res.send(userSpec);
+  });
+  app.use("/swagger/post.json", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(postSpec);
   });
 }
 
