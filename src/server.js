67176b46bf7f4f2ad721640f15c18c242a33dacb
phuocht <phuocht@d-soft.com.vn>
2025-09-17 17:22:44 +0700

    update/change config cors

--- src/server.js
+++ src/server.js
@@ -8,20 +8,12 @@ const methodOverride = require("method-override");
 const app = express();
 const errorHandle = require("./app/middleware/errorHandle");
 const swaggerDocs = require("./swagger");
-
+cors = require("cors");
 const route = require("./routes");
 const db = require("./config/db");
 
 // Condition CORS
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
+app.use(cors());
 
 // Connect to database
 db.connect();
